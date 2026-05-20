ed7c0694fabe5b8a2cb712eca6e0a0e3f75b5f96
Vitooo118 <vitorpablo092@gmail.com>
2026-05-20 11:54:53 -0300

    programa que faz print por meio de ¨flags¨

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        for (int i = 0; i < args.Length; ++i)
+        {
+            switch (args[i])
+            {
+                case("print"):
+                    Console.WriteLine(args[i+1]);
+
+                break;
+            }
+        }
+
+
+    }
+
+}
